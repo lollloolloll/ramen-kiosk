caa3d87661a723a85259559dac104ba705e0353e
skdhu <skdhu3i2@gmail.com>
2025-10-25 09:26:14 +0900

    chore: 불필요한 import문 제거

--- src/app/(admin)/admin/users/columns.tsx
+++ src/app/(admin)/admin/users/columns.tsx
@@ -2,7 +2,6 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
-import { ArrowUpDown } from "lucide-react";
 import { generalUsers, users } from "@drizzle/schema";
 import { deleteGeneralUser, deleteAdminUser } from "@/lib/actions/generalUser";
 import { toast } from "sonner";

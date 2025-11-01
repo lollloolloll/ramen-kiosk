eff72b3956076bc667a24345030edb4b37e68341
skdhu <skdhu3i2@gmail.com>
2025-11-01 14:39:09 +0900

    chore: update drizzle.config.ts to use local database and .env.local for environment variables; modify service worker script for improved asset caching

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,14 +2,13 @@
 
 import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
-dotenv.config({ path: ".env" }); // .env 파일 경로를 명시해주는 것이 더 안전합니다.
-
+dotenv.config({ path: ".env.local" }); // .env 파일 경로를 명시해주는 것이 더 안전합니다.
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
-    // 하드코딩된 url 대신 환경 변수를 사용합니다.
-    url: process.env.DATABASE_URL!,
+    // 루트의 local.db를 사용하도록 직접 지정합니다.
+    url: "local.db",
   },
 } satisfies Config;
